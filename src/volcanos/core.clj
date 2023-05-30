1d6b73e5a519fd87a108a6bba6fc8a97ebfaae56
Vinicios Wentz <vinicios@wentz.io>
2023-05-29 22:17:13 -0300

    parsing last-known-parsed, tectonic-setting, and dominant-rock-type
    Signed-off-by: Vinicios Wentz <vinicios@wentz.io>

--- src/volcanos/core.clj
+++ src/volcanos/core.clj
@@ -1,20 +1,43 @@
 (ns volcanos.core
   (:require [clojure.data.csv :as csv]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
 
 (def csv-file
   (-> "volcanos.csv"
       io/resource
       io/reader))
 
+(defn slash->set [s]
+  (set (map str/trim (str/split s #"/"))))
+
+(defn string->integer [s]
+  (Integer/parseInt s))
+
+(defn string->double [s]
+  (Double/parseDouble s))
+
+(defn parse-eruption-date [date]
+  (if (= "Unknown" date)
+    nil
+    (let [[_ y e] (re-matches #"(\d+) (.+)" date)]
+      (cond
+        (= e "BCE")
+        (- (Integer/parseInt y))
+        (= e "CE")
+        (Integer/parseInt y)
+        :else
+        (throw (ex-info "Could not parse year." {:year date}))))))
+
 (defn parse-numbers [volcano]
   (-> volcano
-      (update :elevation-meter #(Integer/parseInt %))
-      (update :longitude #(Double/parseDouble %))
-      (update :latitude #(Double/parseDouble %))))
+      (update :elevation-meter string->integer)
+      (update :longitude string->double)
+      (update :latitude string->double )
+      (assoc :last-known-parsed (parse-eruption-date (:last-known-eruption volcano)))
+      (update :tectonic-setting slash->set)
+      (update :dominant-rock-type slash->set)))
 
-(defn parse-date [volcanos]
-  true)
 
 (def csv-lines
   (with-open [csv csv-file]
